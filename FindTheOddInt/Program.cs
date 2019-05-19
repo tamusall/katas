efb5093a6be1a45e70823232351b1f1de461b45d
Travis Musall <tamusall@gmail.com>
2019-05-19 19:36:03 -0400

    Solutions for SquareOfSquares and FindTheOddInt

---
+++ FindTheOddInt/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FindTheOddInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sequence = new [] {20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5};
+
+            Console.WriteLine(Kata.find_it(sequence));
+        }
+    }
+
+    class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            for (int i = 0; i < seq.Length; i++)
+            {
+                int count = 0;
+
+                for (int j = 0; j < seq.Length; j++)
+                {
+                    if (seq[i] == seq[j])   
+                    {
+                        count++;
+                    }
+                }
+                if (count % 2 != 0)
+                    return seq[i];
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ SquareOfSquares/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SquareOfSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Is -1 a perfect square?");
+            Console.WriteLine(Kata.IsSquare(-1) ? "yes" : "no");
+            Console.WriteLine("Is 3 a perfect square?");
+            Console.WriteLine(Kata.IsSquare(3) ? "yes" : "no");
+            Console.WriteLine("Is 4 a perfect square?");
+            Console.WriteLine(Kata.IsSquare(4) ? "yes" : "no");
+            Console.WriteLine("Is 25 a perfect square?");
+            Console.WriteLine(Kata.IsSquare(25) ? "yes" : "no");
+            Console.WriteLine("Is 26 a perfect square?");
+            Console.WriteLine(Kata.IsSquare(26) ? "yes" : "no");
+        }
+    }
+
+    public class Kata
+    {
+        public static bool IsSquare(int n)
+        {
+            //find the square root of the number
+            var squareRoot = Math.Sqrt(n);
+            //check if square root is an integer
+            return ((squareRoot - Math.Floor(squareRoot)) == 0);
+        }
+    }
+}
