de8e0c1457f8fc093c17ca124d222448c9b43c64
Travis Musall <tamusall@gmail.com>
2019-05-19 21:19:42 -0400

    Mumbling project Added

---
+++ Mumbling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace Mumbling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Accumul.Accum("ZpglnRxqenU"));
+        }
+    }
+
+    public static class Accumul
+    {
+        public static string Accum(string s)
+        {
+            // var inputArr = s.ToCharArray();
+            // var finalString = new StringBuilder();
+
+            // for (int i = 0; i < inputArr.Length; i++)
+            // {
+            //     finalString.Append(inputArr[i].ToString().ToLower().ToUpper());
+            //     finalString.Append(inputArr[i].ToString().ToLower().Repeat(i));
+            //     if (i < inputArr.Length - 1)
+            //         finalString.Append("-");
+            // }
+            // return finalString.ToString();
+
+            return string.Join("-",s.Select((x,i)=>char.ToUpper(x)+new string(char.ToLower(x),i)));
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string Repeat(this string instr, int n)
+        {
+            var result = string.Empty;
+
+            for (var i = 0; i < n; i++)
+                result += instr;
+
+            return result;
+        }
+    }
+}
